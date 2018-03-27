4a0808d984d831d3c063b5b2f08a00e7012b2cae
pli01 <pli01@github.com>
2018-03-27 13:33:26 +0000

    optionnal lint

--- tests/unit-test.sh
+++ tests/unit-test.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -ex
 
-time ( cd $(dirname $0)/../ansible &&
-  ansible-lint -x ANSIBLE0010  playbooks/*.yml
+time ( cd $(dirname $0)/../ansible
+
+test -x "$(type -p ansible-lint)" && ansible-lint -x ANSIBLE0010 playbooks/*.yml
 
   echo "ansible syntax-check"
   ansible-playbook -i config -c local -l localhost \
