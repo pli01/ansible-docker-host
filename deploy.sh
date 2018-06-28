e71dab06642221a4895316bd0f3aaa59cb07c0b6
pli01 <pli01@github.com>
2018-06-28 12:29:14 +0000

    disable hsot key checking , rename ANSIBLE_DEPLOY_LOCAL to ANSIBLE_DEPLOY_LIMIT

--- deploy.sh
+++ deploy.sh
@@ -8,10 +8,13 @@ export SERVICE_TINYPROXY_ENABLE=${SERVICE_TINYPROXY_ENABLE:-true}
 export SERVICE_OPENSTACK_CLIENT_ENABLE=${SERVICE_OPENSTACK_CLIENT_ENABLE:-true}
 export SERVICE_DOCKER_COMPOSE_ENABLE=${SERVICE_DOCKER_COMPOSE_ENABLE:-true}
 export SERVICE_DOCKER_ENABLE=${SERVICE_DOCKER_ENABLE:-true}
-export ANSIBLE_DEPLOY_LOCAL=${ANSIBLE_DEPLOY_LOCAL:-$(hostname -s)}
+export ANSIBLE_DEPLOY_LIMIT=${ANSIBLE_DEPLOY_LIMIT:-$(hostname -s)}
+# use connection smart for ssh or local
+export ANSIBLE_CONNECTION=${ANSIBLE_CONNECTION:-local}
+export ANSIBLE_HOST_KEY_CHECKING=False
 
 echo "Configuration started"
 time ( cd $(dirname $0)/ansible &&
-  ansible-playbook -i config -c local -l ${ANSIBLE_DEPLOY_LOCAL} \
+  ansible-playbook -i config -c ${ANSIBLE_CONNECTION} -l ${ANSIBLE_DEPLOY_LIMIT} \
      playbooks/site.yml -v
 )
